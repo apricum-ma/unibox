9c062b0756b5e1cbd7616da73fc709b3c2cff509
Apricum Ying-chao Ma <i@yingchao.ma>
2016-06-29 18:50:44 +0800

    Definitions of Meta

--- Unibox/Unibox/BoxApp.cs
+++ Unibox/Unibox/BoxApp.cs
@@ -5,11 +5,38 @@ using System.Text;
 
 namespace Unibox
 {
-    class BoxApp
+    abstract class BoxApp
     {
-        class PatternData
+        public class Meta
         {
-
+            public Dictionary<int, int> Pattern = new Dictionary<int, int>();
+            public Dictionary<int, IEnumerable<string>> Data
+                = new Dictionary<int, IEnumerable<string>>();
+            public static Meta operator +(Meta lVal, Meta rVal)
+            {
+                var ans = (Meta)lVal.MemberwiseClone();
+                foreach (var i in rVal.Pattern)
+                    ans.Pattern[i.Key]
+                        = ans.Pattern.ContainsKey(i.Key)
+                        ? ans.Pattern[i.Key]
+                        : 0
+                        + i.Value;
+                foreach(var i in rVal.Data)
+                {
+                    if (ans.Data.ContainsKey(i.Key))
+                        ans.Data[i.Key] = ans.Data[i.Key].Concat(i.Value);
+                    else
+                        ans.Data[i.Key] = i.Value;
+                }
+                return ans;
+            }
+        }
+        public abstract Meta GetMeta(string word);
+        public Meta GetMeta(IEnumerable<string> words)
+        {
+            var ans = new Meta();
+            foreach (string word in words) ans += GetMeta(word);
+            return ans;
         }
     }
     class SampleApp :BoxApp
